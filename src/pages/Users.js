8080dd456a16dedecf975b72abc726570d16095a
masoume <masume.ndri@gmail.com>
2022-06-04 16:08:24 +0430

    done :)

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -2,17 +2,30 @@ import React, { useState } from "react";
 import Login from "./Login";
 import Signin from "./Signin";
 import styled from "styled-components";
+const BtnContainer = styled("div")`
+   width: 100%;
+   margin: auto;
+   text-align: center;
+`
 const Button = styled("button")`
-   padding: 8px 30px;
-   border-radius: 20px;
+   margin-top: 1rem;
+   padding: 10px 50px;
+   border-radius: 30px;
    border: none;
+   background: #eaafc8;
+   color: white;
+   font-size: 16px;
 `;
 const Users = () => {
    const [isLogin, setIsLogin] = useState(true);
 
    return (
       <>
-         <Button onClick={() => setIsLogin(!isLogin)}>{isLogin?"register":"login"}</Button>
+         <BtnContainer>
+            <Button onClick={() => setIsLogin(!isLogin)}>
+               {isLogin ? "register" : "login"}
+            </Button>
+         </BtnContainer>
          {isLogin ? <Login /> : <Signin />}
       </>
    );
